108173e9250ff5e8e4672993d70ead219a4f35f7
Cheyenne M <76500260+martiche93@users.noreply.github.com>
2021-08-27 07:00:21 -0700

    Updated Code

--- app.js
+++ app.js
@@ -2,84 +2,229 @@
 // Activity 15.3.8 Reference
 // ----------------------------
 
+// TRACE ONE
 // Use samples.json to fetch data from JSON file
 // Incoming data is internally referred to as incomingData
 d3.json("samples.json").then((incomingData) => {
-    function filterTumorRating(tumor) {
+    // Function to filter the radius_mean data greater than 0
+    function filterTumorRadius(tumor) {
         return tumor.radius_mean > 0;
     }
 
     // Use filter() to pass the function as its argument
-    var filteredTumors = incomingData.filter(filterTumorRating);
+    var filteredRadius = incomingData.filter(filterTumorRadius);
 
-    // Check to make sure you are filtering the tumors
-    console.log(filteredTumors);
+    // Check to make sure you are filtering the tumors by radius
+    console.log(filteredRadius);
 
-    // Use the map method with the arrow function to return all the filtered tumor radius means for the x-axis
-    var radiusMeans = filteredTumors.map(tumors => tumors.radius_mean);
-
-    // Use the map method with the arrow function to return all the filtered tumor texture means for the x-axis
-    var textureMeans = filteredTumors.map(tumors => tumors.texture_mean);
-
-    // Use the map method with the arrow function to return all the filtered tumor texture means for the x-axis
-    var perimeterMeans = filteredTumors.map(tumors => tumors.perimeter_mean);
-
-    // Use the map method with the arrow function to return all the filtered tumor texture means for the x-axis
-    var areaMeans = filteredTumors.map(tumors => tumors.area_mean);
-
-    // Use the map method with the arrow function to return all the filtered tumor smoothness means for the x-axis
-    var smoothnessMeans = filteredTumors.map(tumors => tumors.smoothness_mean);
-
-    // Use the map method with the arrow function to return all the filtered tumor compactness means for the x-axis
-    var compactnessMeans = filteredTumors.map(tumors => tumors.compactness_mean);
-
-    // Use the map method with the arrow function to return all the filtered tumor texture means for the x-axis
-    var concavityMeans = filteredTumors.map(tumors => tumors.concavity_mean);
-
-    // Use the map method with the arrow function to return all the filtered tumor texture means for the x-axis
-    var concavePointsMeans = filteredTumors.map(tumors => tumors.concave_points_mean);
-
-    // Use the map method with the arrow function to return all the filtered tumor texture means for the x-axis
-    var symmetryMeans = filteredTumors.map(tumors => tumors.symmetry_mean);
-
-    // Use the map method with the arrow function to return all the filtered tumor texture means for the x-axis
-    var fractalDimensionMeans = filteredTumors.map(tumors => tumors.fractal_dimension_mean);
+    // Use the map method with the arrow function to return all the filtered diagnosis for M or B
+    var ids = filteredRadius.map(tumors => tumors.id);
 
     // Use the map method with the arrow function to return all the filtered diagnosis types for the y-axis
-    var types = filteredTumors.map(tumors => tumors.diagnosis);
+    var ratings = filteredRadius.map(tumors => tumors.diagnosis);
 
     // Check your filtered radius means
-    console.log(types);
+    console.log(ratings);
 
     // Create your trace
     var trace1 = {
-        x: {radiusMeans,
-            textureMeans,
-            perimeterMeans,
-            areaMeans,
-            smoothnessMeans,
-            compactnessMeans,
-            concavityMeans,
-            concavePointsMeans,
-            symmetryMeans,
-            fractalDimensionMeans},
-        y: types,
-        text: ['A<br>size: 40', 'B<br>size: 60', 'C<br>size: 80', 'D<br>size: 100'],
+        x: ids,
+        y: ratings,
+        text: ['Radius (Avg)'],
         mode: 'markers',
         marker: {
-            color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
-            opacity: [1, 0.8, 0.6, 0.4],
-            size: [40, 60, 80, 100]
+            color: ['red'],
+            size: [200]
         }
     };
 
-    var data = [trace1];
+    // TRACE TWO
+    function filterTumorTexture(tumor) {
+        return tumor.radius_mean > 0;
+    }
+    var filteredTexture = incomingData.filter(filterTumorTexture);
+
+    var ids = filteredTexture.map(tumors => tumors.id);
+    var ratings = filteredTexture.map(tumors => tumors.diagnosis);
+
+    var trace2 = {
+        x: ids,
+        y: ratings,
+        text: ['Texture (Avg)'],
+        mode: 'markers',
+        marker: {
+            color: ['pink'],
+            size: [175]
+        }
+    };
+
+    // TRACE THREE
+    function filterTumorPerimeter(tumor) {
+        return tumor.perimeter_mean > 0;
+        }
+    var filteredPerimeter = incomingData.filter(filterTumorPerimeter);
+
+    var ids = filteredPerimeter.map(tumors => tumors.id);
+    var ratings = filteredPerimeter.map(tumors => tumors.diagnosis);
+
+    var trace3 = {
+        x: ids,
+        y: ratings,
+        text: ['Perimeter (Avg)'],
+        mode: 'markers',
+        marker: {
+            color: ['orange'],
+            size: [150]
+        }
+    };
+
+    // TRACE FOUR
+    function filterTumorArea(tumor) {
+        return tumor.area_mean > 0;
+    }   
+    var filteredArea = incomingData.filter(filterTumorArea);
+
+    var ids = filteredArea.map(tumors => tumors.id);
+    var ratings = filteredArea.map(tumors => tumors.diagnosis);
+
+    var trace4 = {
+        x: ids,
+        y: ratings,
+        text: ['Area (Avg)'],
+        mode: 'markers',
+        marker: {
+            color: ['yellow'],
+            size: [125]
+        }
+    };
+
+    // TRACE FIVE
+    function filterTumorSmoothness(tumor) {
+        return tumor.smoothness_mean > 0;
+    }
+    var filteredSmoothness = incomingData.filter(filterTumorSmoothness);
+
+    var ids = filteredSmoothness.map(tumors => tumors.id);
+    var ratings = filteredSmoothness.map(tumors => tumors.diagnosis);
+
+    var trace5 = {
+        x: ids,
+        y: ratings,
+        text: ['Smoothness (Avg)'],
+        mode: 'markers',
+        marker: {
+            color: ['green'],
+            size: [100]
+        }
+    };
+
+    // TRACE SIX
+    function filterTumorCompactness(tumor) {
+        return tumor.compactness_mean > 0;
+    }
+    var filteredCompactness = incomingData.filter(filterTumorCompactness);
+
+    var ids = filteredCompactness.map(tumors => tumors.id);
+    var ratings = filteredCompactness.map(tumors => tumors.diagnosis);
+
+    var trace6 = {
+        x: ids,
+        y: ratings,
+        text: ['Compactness (Avg)'],
+        mode: 'markers',
+        marker: {
+            color: ['lightblue'],
+            size: [75]
+        }
+    };
+
+    // TRACE SEVEN
+    function filterTumorConcavity(tumor) {
+        return tumor.concavity_mean > 0;
+    }
+    var filteredConcavity = incomingData.filter(filterTumorConcavity);
+
+    var ids = filteredConcavity.map(tumors => tumors.id);
+    var ratings = filteredConcavity.map(tumors => tumors.diagnosis);
+
+    var trace7 = {
+        x: ids,
+        y: ratings,
+        text: ['Concavity (Avg)'],
+        mode: 'markers',
+        marker: {
+            color: ['blue'],
+            size: [50]
+        }
+    };
+
+    // TRACE EIGHT
+    function filterTumorConcavePoints(tumor) {
+        return tumor.concave_points_mean > 0;
+    }
+    var filteredConcavePoints = incomingData.filter(filterTumorConcavePoints);
+
+    var ids = filteredConcavePoints.map(tumors => tumors.id);
+    var ratings = filteredConcavePoints.map(tumors => tumors.diagnosis);
+
+    var trace8 = {
+        x: ids,
+        y: ratings,
+        text: ['Concave Points (Avg)'],
+        mode: 'markers',
+        marker: {
+            color: ['lavendar'],
+            size: [25]
+        }
+    };
+
+    // TRACE NINE
+    function filterTumorSymmetry(tumor) {
+        return tumor.symmetry_mean > 0;
+    }
+    var filteredSymmetry = incomingData.filter(filterTumorSymmetry);
+
+    var ids = filteredSymmetry.map(tumors => tumors.id);
+    var ratings = filteredSymmetry.map(tumors => tumors.diagnosis);
+
+    var trace9 = {
+        x: ids,
+        y: ratings,
+        text: ['Symmetry (Avg)'],
+        mode: 'markers',
+        marker: {
+            color: ['purple'],
+            size: [25]
+        }
+    };
+
+    // TRACE TEN
+    function filterTumorFractalDimension(tumor) {
+        return tumor.fractal_dimension_mean > 0;
+    }
+    var filteredFractalDimension = incomingData.filter(filterTumorFractalDimension);
+
+    var ids = filteredFractalDimension.map(tumors => tumors.id);
+    var ratings = filteredFractalDimension.map(tumors => tumors.diagnosis);
+
+    var trace10 = {
+        x: ids,
+        y: ratings,
+        text: ['Fractal Dimensions (Avg)'],
+        mode: 'markers',
+        marker: {
+            color: ['black'],
+            size: [5]
+        }
+    };
+
+    var data = [trace1, trace2, trace3, trace4, trace5, trace6, trace7, trace8, trace9, trace10];
 
     var layout = {
-        title: 'Marker Size',
-        showlegend: false,
-        height: 600,
-        width: 600
+        title: 'Benign vs Malignant',
+        showlegend: true,
+        height: 700,
+        width: 700
     };
 
     Plotly.newPlot("bubble", data, layout);
