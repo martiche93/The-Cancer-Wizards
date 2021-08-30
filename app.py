30f5838a46f0ee8d82bee6e5f2f9fc006fb33aa5
rls1316 <80739270+rls1316@users.noreply.github.com>
2021-08-29 21:44:52 -0700

    updated for heroku deploy

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
 # import all dependencies
-from flask import Flask, render_template, request, redirect, jsonify
+from flask import Flask, render_template, request, redirect, jsonify, url_for
 
 app = Flask(__name__)
 
 
 # Homepage route
 @app.route("/")
-def homepage():
+def home():
+    app.route('/')
     # use return render_template to access ALL html links
-    return "Put index.html file here!"
+    return render_template("index.html")
+
+
+@app.route("/About")
+def about():
+    return render_template("about.html")
 
 
 # Analysis route
 @app.route("/Analysis")
 def analysis():
-    return "Put analysis.html file here!"
+    return render_template("analysis.html")
 
 
 # Machine learning route
@@ -24,24 +30,30 @@ def machine_learning():
 
 
 # Here are the individual routes for the Machine Learning link
-@app.route("/ML/LogisticRegressionModel")
-def log_reg_model():
-    return "Put Logistic Regression html file here!"
+@app.route("/ML/Models")
+def models():
+    return render_template("models.html")
 
 
-@app.route("/ML/SVMModel")
-def SVM_model():
-    return "Put SVM Model html file here!"
-
-
+# Here are the individual routes for the Machine Learning link
 @app.route("/ML/KernelSVMModel")
 def Kernel_SVM_model():
-    return "Put Kernel SVM html file here!"
+    return render_template("model1.html")
+
+
+@app.route("/ML/LogisticRegressionModel")
+def log_reg_model():
+    return render_template("model2.html")
 
 
 @app.route("/ML/RandomForestModel")
 def Random_Forest_model():
-    return "Put Random Forest html file here!"
+    return render_template("model3.html")
+    
+    
+@app.route("/ML/SVMModel")
+def SVM_model():
+    return render_template("model4.html")
 
 
 # Tableau route
@@ -53,7 +65,7 @@ def Tableau_Dashboard():
 # Conclusion route
 @app.route("/Conclusion")
 def conclusion():
-    return "Put conclusion.html file here!"
+    return render_template("conclusions.html")
 
 
 if __name__ == "__main__":
